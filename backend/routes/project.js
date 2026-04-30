9eff8bced3cf1a6e37672f4072c469e1ef7b2115
WatchJani <jankokondic84@gmail.com>
2026-04-30 16:21:36 +0200

    create project endpoint

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -5,5 +5,61 @@ const multer = require('multer');
 
 let upload_dest = multer({ dest: 'uploads/' });
 
+project.post('/', upload_dest.fields([
+    { name: 'thumbnail', maxCount: 1 },
+    { name: 'images', maxCount: 10 }
+]), async (req, res) => {
+
+    if (!req.session || !req.session.user) {
+        return res.status(401).json({ error: 'User is not loged in.' });
+    }
+
+    const {
+        title,
+        description,
+        category,
+        difficulty,
+        time_required,
+        is_published,
+        instruction,
+        materials
+    } = req.body;
+
+    if (!title || !description || !category || !difficulty || !time_required || !instruction) {
+        return res.status(400).json({ error: 'All required fields must be filled in.' });
+    }
+
+    console.log(materials)
+    const parsedMaterials = materials ? JSON.parse(materials) : [];
+    console.log(parsedMaterials)
+
+
+    const thumbnailFile = req.files['thumbnail']?.[0];
+    const imageFiles = req.files['images'] || [];
+
+    const thumbnailPath = thumbnailFile ? `/uploads/${thumbnailFile.filename}` : null;
+    const imagePaths = imageFiles.map(file => `/uploads/${file.filename}`);
+
+    try {
+        const projectId = await DB.CreateProject({
+            title,
+            description,
+            category,
+            difficulty,
+            time_required,
+            is_published: is_published === 'true' ? true : false,
+            instruction,
+            thumbnail: thumbnailPath,
+            user: req.session.user.user_id
+        }, parsedMaterials, imagePaths);
+
+        res.status(201).json({ message: 'Project successfully created.', project_id: projectId });
+    } catch (err) {
+        console.error("Error adding project:", err);
+        res.status(500).json({ error: 'Failed to create project.' });
+    }
+});
+
+
 
 module.exports = project;
