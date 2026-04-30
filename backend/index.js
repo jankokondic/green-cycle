487139b68b731b80404a30ff6d8d41ecab500b3b
WatchJani <jankokondic84@gmail.com>
2026-04-30 14:38:13 +0200

    app configuration

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const app = express()
 const cors = require("cors")
 const path = require('path')
 const port = process.env.PORT || 8080
@@ -7,5 +8,34 @@ const port = process.env.PORT || 8080
 // import local files
 const user = require('./routes/user')
 
-const app = express()
+const session = require('express-session')
+
+app.set('trust proxy', 1) // trust first proxy
+
+// configurations
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://88.200.63.148:8081',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+}));
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax',
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}));
+
+
+app.use('/api/v1/user', user)
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
