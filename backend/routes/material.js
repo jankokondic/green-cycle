041460f4be54cf98917f0e105119ead307e1246a
WatchJani <jankokondic84@gmail.com>
2026-04-30 18:52:54 +0200

    add material handler

--- backend/routes/material.js
+++ backend/routes/material.js
@@ -7,6 +7,43 @@ const multer = require('multer')
 let upload_dest = multer({ dest: 'uploads/' })
 
 
+material.post('/add', upload_dest.single('file'), async (req, res) => {
+    const {
+        category,
+        description,
+        is_ecologically,
+        is_sensitive,
+        unit,
+        name
+    } = req.body;
+    const file = req.file;
+
+    const is_ecologically_bool = is_ecologically === 'true' ? true : false;
+    const is_sensitive_bool = is_sensitive === 'true' ? true : false;
+
+    if (!category || !description || !unit || !name || !file) {
+        return res.status(400).json({ error: 'All fields including file are required.' });
+    }
+
+    file_name = `/uploads/${file.filename}`;
+
+    try {
+        await DB.AddMaterial({
+            category,
+            description,
+            is_ecologically_bool,
+            is_sensitive_bool,
+            unit,
+            name,
+            file_name,
+        });
+
+        res.status(201).json({ message: 'Material successfully added.' });
+    } catch (err) {
+        console.error("Error adding material:", err);
+        res.status(500).json({ error: 'Server error while adding material.' });
+    }
+});
 
 
 
